a81005749762529f2ac97be0d8c4ce6a1d93b8e1
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-06-20 10:15:14 -0500

    VENTANAS JAVA

--- src/PanelDibujo.java
+++ src/PanelDibujo.java
@@ -1,42 +1,57 @@
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class PanelDibujo extends JPanel{
+    int anchura = 100;
+    int altura = 100;
+
+    double peso;
+    double estatura;
+    double imc;
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int anchura = 100;
-        int altura = 100;
+        Scanner sc = new Scanner(System.in);
 
         //seterar las lineas
-        g.drawLine(50,50,50,100);
-        g.drawLine(50,50,100,50);
-        g.drawLine(50,75,100, 75);
-        g.drawLine(50,100,100,100);
+        //g.drawLine(50,50,50,100);
+        //g.drawLine(50,50,100,50);
+        //g.drawLine(50,75,100, 75);
+        //g.drawLine(50,100,100,100);
 
         //dibujo un circulo
+        /*
         g.drawRoundRect(0,0,50,50,30,30);
 
         //crea la palabra en la posicion que la pongas
         g.drawString("Eduardo", 150, 150);
 
         //Utilizndo el metodo draw string, dibuje una ventana el nombre del usuario que ingrese por consola
-        Scanner sc = new Scanner(System.in);
+
         System.out.println("Ingresa el nombre: ");
         String nombre = sc.nextLine();
 
         g.drawString(nombre, 250, 250);
+        */
 
-        //ejercicio del imc
-        int peso = 23;
-        int estatura = 34;
-        int imc;
+        //ejercicio del IMC
+        g.drawString("-----IMC-----", 10, 10);
+        System.out.println("Ingrese su peso: ");
+        peso = sc.nextDouble();
+        sc.nextLine();
+        g.drawString("Tu peso es: " + peso, 10, 20);
+        System.out.println("Ingrese su altura: ");
+        estatura = sc.nextDouble();
+        sc.nextLine();
+        g.drawString("Tu estatura es" + estatura, 10, 30);
+        imc = peso * (altura * altura);
+        g.drawString("Tu IMC es: " + imc, 10, 40);
 
-        imc = peso * (estatura * estatura);
 
-        String imcc = Integer.toString(imc);
 
-        g.drawString(imcc, 600, 600);
+
+
     }
 
 
